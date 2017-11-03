fdda5a03a66889db5f6b6f20939e809ab5374258
Kayleen Fulton <kseebug@Marseilles.local>
2017-11-03 14:24:08 -0700

    This is the first commit of my database file. Im sooooooo excited!

---
+++ bSi_dataframe.py
@@ -0,0 +1,5 @@
+conversion_list = {'A':1.375,'B':2.625,'C':5.965909091,'D':1,'E':1.875,'F':6.25,'G':9375,'H':5.25,'I':7.5}
+poly_a = -6.84578043712098E-06
+poly_b = 0.0114955100658752
+poly_c = 0.000423122081959965
+for row in a.iterrows()  #loops through rows
